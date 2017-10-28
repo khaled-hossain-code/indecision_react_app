078042e81fbd49d72787707de3713cdf2c45e4d1
khaled <md.khaled.hossain@gmail.com>
2017-10-28 20:54:13 +0600

    indecision app almost done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,87 @@
 'use strict';
 
 console.log('Inside src app');
-
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Your decision Computer will take',
+  options: []
+};
 var appRoot = document.getElementById('app');
-var template = React.createElement(
-  'h1',
-  null,
-  'Babel can Watch!'
-);
 
-ReactDOM.render(template, appRoot);
+var removeAll = function removeAll() {
+  app.options = [];
+  renderApp();
+};
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderApp();
+  }
+};
+
+var decide = function decide() {
+  var randomNum = Math.floor(Math.random() * app.options.length);
+  var option = app.options[randomNum];
+  alert(option);
+};
+
+var renderApp = function renderApp() {
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length > 0 ? 'Here are your Options' : 'No options'
+    ),
+    React.createElement(
+      'button',
+      { disabled: app.options.length === 0, onClick: decide },
+      'What Should I do? '
+    ),
+    React.createElement(
+      'button',
+      { onClick: removeAll },
+      ' Remove All '
+    ),
+    React.createElement(
+      'ul',
+      null,
+      app.options.map(function (opt) {
+        return React.createElement(
+          'li',
+          { key: opt },
+          opt
+        );
+      })
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        ' Add Option '
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+renderApp();
--- src/app.js
+++ src/app.js
@@ -1,6 +1,53 @@
 console.log('Inside src app');
-
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Your decision Computer will take',
+  options:[]
+}
 var appRoot = document.getElementById('app');
-var template = <h1>Babel can Watch!</h1>;
 
-ReactDOM.render(template, appRoot);
+const removeAll = () => {
+  app.options = [];
+  renderApp();
+}
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+
+  if(option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderApp();
+  }
+}
+
+const decide = () => {
+  let randomNum = Math.floor( Math.random() * app.options.length);
+  let option = app.options[randomNum];
+  alert(option);
+}
+
+const renderApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your Options' : 'No options'}</p>
+      <button disabled={ app.options.length === 0 } onClick={decide}>What Should I do? </button>
+      <button onClick={removeAll}> Remove All </button>
+      <ul>
+        {
+          app.options.map( opt => <li key={opt}>{opt}</li> )
+        }
+      </ul>
+      <form onSubmit={onFormSubmit}>
+      <input type="text" name="option" />
+      <button> Add Option </button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+}
+
+renderApp();
